c68fb3bc1d8e2626b96d787593a29d5216c28d9c
Ceramic Soda <dmch.d89@gmail.com>
2023-07-17 02:23:09 +0300

    Gltf loading

--- main.js
+++ main.js
@@ -8,23 +8,24 @@ let loader = new GLTFLoader();
 let camera = new THREE.PerspectiveCamera(30, window.innerWidth/window.innerHeight, 0.001, 1000);
 let renderer = new THREE.WebGLRenderer({alpha: true});
 let controls = new OrbitControls(camera, renderer.domElement);
-let bomb = new THREE.Group();
+let bomb = {group: new THREE.Group()};
 //-----------------------------GLTF
 loader.loadAsync("assets/bomb.glb")
     .catch(err => console.error(err))
     .then(gltf => {
-      scene.add(gltf.scene);
+      bomb.body = gltf.scene.getObjectByName("bomb");
+      bomb.fuze = gltf.scene.getObjectByName("fuze");
+      bomb.cap = gltf.scene.getObjectByName("cap");
+      bomb.group.add(bomb.body,bomb.fuze,bomb.cap);
   })
 //-----------------------------INIT
 document.body.appendChild(renderer.domElement); 
 renderer.setAnimationLoop(animate);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.set(10,1,0);
+camera.position.set(4,2,0);
 camera.lookAt(0,0,0);
 let alight = new THREE.AmbientLight(0x888888);
-let sphere = new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshBasicMaterial(0x432fd6));
-scene.add(alight);
-console.log(scene);
+scene.add(bomb.group, alight);
 //-----------------------------LOOP
 function animate () {
   controls.update(); 
