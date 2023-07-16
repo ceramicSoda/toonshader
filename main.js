445a2858c373c68bf5d9a28cd3bd463d7057d4d6
Ceramic Soda <dmch.d89@gmail.com>
2023-07-17 02:08:13 +0300

    mesh loading

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import './style.css';
 //-----------------------------THREE 
 let scene = new THREE.Scene();
 let loader = new GLTFLoader();
-let camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.001, 1000);
+let camera = new THREE.PerspectiveCamera(30, window.innerWidth/window.innerHeight, 0.001, 1000);
 let renderer = new THREE.WebGLRenderer({alpha: true});
 let controls = new OrbitControls(camera, renderer.domElement);
 let bomb = new THREE.Group();
@@ -18,9 +18,15 @@ loader.loadAsync("assets/bomb.glb")
 //-----------------------------INIT
 document.body.appendChild(renderer.domElement); 
 renderer.setAnimationLoop(animate);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.set(10,1,0);
+camera.lookAt(0,0,0);
+let alight = new THREE.AmbientLight(0x888888);
+let sphere = new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshBasicMaterial(0x432fd6));
+scene.add(alight);
+console.log(scene);
 //-----------------------------LOOP
 function animate () {
-  
   controls.update(); 
   renderer.render(scene, camera); 
 }
