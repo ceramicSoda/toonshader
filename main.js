6ee4e8df6adb6ba3839f005ce2b754954f405844
Ceramic Soda <dmch.d89@gmail.com>
2023-07-17 01:18:47 +0300

    Basic scene

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+import * as THREE from 'three';
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import './style.css';
+//-----------------------------THREE 
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.001, 1000);
+let renderer = new THREE.WebGLRenderer({alpha: true});
+let controls = new OrbitControls(camera, renderer.domElement);
+//-----------------------------INIT
+document.body.appendChild(renderer.domElement); 
+renderer.setAnimationLoop(animate);
+//-----------------------------LOOP
+function animate () {
+  
+  controls.update(); 
+  renderer.render(scene, camera); 
+}
+//------------------------------RESIZE
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+})
