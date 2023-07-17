3731bff5590f59609be57062ffdcec1c43ac7a3b
Ceramic Soda <dmch.d89@gmail.com>
2023-07-17 21:45:41 +0300

    minor improvements

--- fuzeShader.js
+++ fuzeShader.js
@@ -6,14 +6,15 @@ export const fuzeVS = /*glsl*/`
     }
 `
 export const fuzeFS = /*glsl*/`
+    #include <common> 
+    #include <lights_pars_begin>
     uniform vec3    uColor;
     uniform float   uState; 
     varying vec2    vUV;
     void main(){
-        vec3 cLight = vec3(uColor.r + 1., uColor.g + .1, uColor.b + .1);
+        vec3 cLight = vec3(uColor.r + 1., uColor.g + .1, uColor.b);
         vec4 c = vec4(uColor * step(vUV.y, (uState)), 1.); 
         c = c + vec4(cLight * step(uState, vUV.y) * step(vUV.y, (uState + 0.15)),  1.);
-        //vec4 c = vec4(uColor * smoothstep(vUV.y, (uState + 0.2), uState) * step(vUV.y, uState), 1.); 
         gl_FragColor = vec4( pow(c.xyz,vec3(0.454545)), c.w );
     }
 `
--- main.js
+++ main.js
@@ -5,15 +5,27 @@ import { mainVS, mainFS } from "./mainShader.js";
 import { fuzeVS, fuzeFS } from "./fuzeShader.js";
 import './style.css';
 //-----------------------------THREE 
-let scene = new THREE.Scene();
-let loader = new GLTFLoader();
-let camera = new THREE.PerspectiveCamera(30, window.innerWidth/window.innerHeight, 0.001, 1000);
-let renderer = new THREE.WebGLRenderer({alpha: true});
-let controls = new OrbitControls(camera, renderer.domElement);
-let bomb = {group: new THREE.Group()};
+const scene = new THREE.Scene();
+const loader = new GLTFLoader();
+const camera = new THREE.PerspectiveCamera(30, window.innerWidth/window.innerHeight, 0.001, 1000);
+const renderer = new THREE.WebGLRenderer({alpha: true});
+const controls = new OrbitControls(camera, renderer.domElement);
+const bomb = {group: new THREE.Group()};
+const alight = new THREE.AmbientLight(0x888888, 0.1);
+const dlight01 = new THREE.DirectionalLight(0xcccccc, 0.8);
 //-----------------------------MATERIALS
-let fuzeMat = new THREE.ShaderMaterial({
+const mainMat = new THREE.ShaderMaterial({
+  lights: true,
   uniforms: {
+    ...THREE.UniformsLib.lights, 
+    uColor: {value: new THREE.Color(0x554c9e)}},
+  vertexShader: mainVS,
+  fragmentShader: mainFS
+})
+const fuzeMat = new THREE.ShaderMaterial({
+  lights: true,
+  uniforms: {
+    ...THREE.UniformsLib.lights, 
     uColor: {value: new THREE.Color(0xAA9060)},
     uState: {type: "f", value: 0.8},},
   vertexShader: fuzeVS,
@@ -27,7 +39,8 @@ loader.loadAsync("assets/bomb.glb")
     bomb.fuze = gltf.scene.getObjectByName("fuze");
     bomb.cap = gltf.scene.getObjectByName("cap");
     bomb.group.add(bomb.body,bomb.fuze,bomb.cap);
-    bomb.body.material = new THREE.MeshToonMaterial({color: 0x554c9e});
+    //bomb.body.material = new THREE.MeshToonMaterial({color: 0x554c9e});
+    bomb.body.material = mainMat;
     bomb.fuze.material = fuzeMat;
     bomb.cap.material = new THREE.MeshToonMaterial({color: 0xA8A8A8});
 })
@@ -37,8 +50,6 @@ renderer.setAnimationLoop(animate);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.set(4,2,0);
 camera.lookAt(0,0,0);
-let alight = new THREE.AmbientLight(0x888888, 0.1);
-let dlight01 = new THREE.DirectionalLight(0xcccccc, 0.8);
 dlight01.position.set(4,2,4);
 dlight01.lookAt(0,0,0);
 scene.add(bomb.group, alight, dlight01);
@@ -46,7 +57,7 @@ scene.add(bomb.group, alight, dlight01);
 function animate () {
   fuzeMat.uniforms.uState.value < 0
     ? fuzeMat.uniforms.uState.value = 1
-    : fuzeMat.uniforms.uState.value -= 0.005;
+    : fuzeMat.uniforms.uState.value -= 0.002;
   controls.update(); 
   renderer.render(scene, camera); 
 }
