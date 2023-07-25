cf6e05dc0b918048e8e9f77fdff97aa86407d115
Ceramic Soda <dmch.d89@gmail.com>
2023-07-25 05:30:56 +0300

    Specular Shader

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { mainVS, mainFS } from "./mainShader.js";
 import { fuzeVS, fuzeFS } from "./fuzeShader.js";
 import './style.css';
-//-----------------------------THREE 
+// three
 const scene = new THREE.Scene();
 const loader = new GLTFLoader();
 const camera = new THREE.PerspectiveCamera(30, window.innerWidth/window.innerHeight, 0.001, 1000);
@@ -13,7 +13,8 @@ const controls = new OrbitControls(camera, renderer.domElement);
 const bomb = {group: new THREE.Group()};
 const alight = new THREE.AmbientLight(0x888888, 0.1);
 const dlight01 = new THREE.DirectionalLight(0xcccccc, 0.8);
-//-----------------------------MATERIALS
+const plight01 = new THREE.PointLight(0xcccccc, 0.8);
+// Materials
 const mainMat = new THREE.ShaderMaterial({
   lights: true,
   uniforms: {
@@ -31,7 +32,7 @@ const fuzeMat = new THREE.ShaderMaterial({
   vertexShader: fuzeVS,
   fragmentShader: fuzeFS
 })
-//-----------------------------GLTF
+// Asset loading
 loader.loadAsync("assets/bomb.glb")
   .catch(err => console.error(err))
   .then(gltf => {
@@ -39,12 +40,12 @@ loader.loadAsync("assets/bomb.glb")
     bomb.fuze = gltf.scene.getObjectByName("fuze");
     bomb.cap = gltf.scene.getObjectByName("cap");
     bomb.group.add(bomb.body,bomb.fuze,bomb.cap);
-    //bomb.body.material = new THREE.MeshToonMaterial({color: 0x554c9e});
     bomb.body.material = mainMat;
+    bomb.body.material = new THREE.MeshToonMaterial({color: 0x554c9e});
     bomb.fuze.material = fuzeMat;
     bomb.cap.material = new THREE.MeshToonMaterial({color: 0xA8A8A8});
 })
-//-----------------------------INIT
+// Init
 document.body.appendChild(renderer.domElement); 
 renderer.setAnimationLoop(animate);
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -52,8 +53,8 @@ camera.position.set(4,2,0);
 camera.lookAt(0,0,0);
 dlight01.position.set(4,2,4);
 dlight01.lookAt(0,0,0);
-scene.add(bomb.group, alight, dlight01);
-//-----------------------------LOOP
+scene.add(bomb.group, alight, dlight01, plight01);
+// Loop
 function animate () {
   fuzeMat.uniforms.uState.value < 0
     ? fuzeMat.uniforms.uState.value = 1
@@ -61,7 +62,7 @@ function animate () {
   controls.update(); 
   renderer.render(scene, camera); 
 }
-//------------------------------RESIZE
+// Events
 window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
